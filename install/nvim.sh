af13cf5588b8b70142b84e315c06969f7154eed3
Cadu Higueras <cadu.higueras@gmail.com>
2025-11-26 21:14:26 +0000

    Add install nvim and obsidian from repo

---
+++ install/nvim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p /home/arch/files/temp/nvim/test
+git clone git@github.com:caduhigueras/nvim2.conf.git /home/arch/files/temp/nvim/test
---
+++ install/obsidian.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone git@github.com:caduhigueras/obsidian.git /home/arch/.local/share/obsidian
