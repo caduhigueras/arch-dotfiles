0d18d31086a777da14d1c7ce6b3ff01edb79413a
Cadu Higueras <cadu.higueras@gmail.com>
2025-12-03 22:29:36 +0000

    Waybar ricing and some install shells

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm uwsm
+
+
+##################################
+#              CRON              #
+##################################
+
+# Instal Cron implementation
+sudo pacman -S --needed --noconfirm cronie
+
+# Add it to systemd
+sudo systemctl enable cronie.service
+sudo systemctl start cronie.service
+
+
+##################################
+#          PROGRAMING            #
+##################################
+# TODO: add here: docker, mysql client, rust toolchain, python, go, php, node and npm
+
--- install/obsidian.sh
+++ install/obsidian.sh
@@ -2,3 +2,21 @@
 
 sudo pacman -S --needed --noconfirm obsidian
 git clone git@github.com:caduhigueras/obsidian.git /home/arch/.local/share/obsidian
+
+sudo touch /usr/local/bin/obsidian_sync
+sudo chmod +x /usr/local/bin/obsidian_sync
+
+sudo tee /usr/local/bin/obsidian_sync > /dev/null <<'EOF'
+#!/bin/bash
+
+cd /home/arch/.local/share/obsidian/
+git fetch --all
+git add .
+git commit -m "Changes from home"
+git push
+EOF
+
+sudo tee /etc/cron.d/sync_obsidian > /dev/null <<'EOF'
+0 * * * * /usr/local/bin/obsidian_sync
+EOF
+
