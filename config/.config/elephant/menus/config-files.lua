7ebf6c83dfaf5968dbe158d30d42ba5cf288ea79
Cadu Higueras <cadu.higueras@gmail.com>
2026-01-07 20:11:45 +0000

    Add / update menus

---
+++ config/.config/elephant/menus/config-files.lua
@@ -0,0 +1,46 @@
+Name = "config_files_menu"
+NamePretty = "Config Files Menu"
+-- Icon = "applications-other"
+Cache = true
+HideFromProviderlist = false
+Description = "List all keybindings configured in Hyprland"
+SearchName = true
+
+local json = require("dkjson") -- use json as the variable
+
+local function read_file(path)
+	local f, err = io.open(path, "r")
+	if not f then
+		return nil, ("Failed to open file: %s"):format(err)
+	end
+
+	local content = f:read("*a")
+	f:close()
+	return content
+end
+
+local function read_json(path)
+	local content, err = read_file(path)
+	if not content then
+		return nil, err
+	end
+
+	local obj, pos, decode_err = json.decode(content, 1, nil)
+	if decode_err then
+		return nil, ("Invalid JSON: %s"):format(decode_err)
+	end
+
+	return obj
+end
+
+function GetEntries()
+	local config_map = "/home/arch/.local/share/arch-dotfiles/config/.config/walker/json/config_files.json"
+	local entries = {}
+
+	local entries, err = read_json(config_map)
+	if not entries then
+		error(err)
+	end
+
+	return entries
+end
---
+++ config/.config/elephant/menus/nvim-shortcuts-lsp.lua
@@ -0,0 +1,46 @@
+Name = "nvim_shortcuts_lsp"
+NamePretty = "Lsp Shortcuts"
+-- Icon = "applications-other"
+Cache = true
+HideFromProviderlist = false
+Description = "List shortcuts from the LSP"
+SearchName = true
+
+local json = require("dkjson") -- use json as the variable
+
+local function read_file(path)
+	local f, err = io.open(path, "r")
+	if not f then
+		return nil, ("Failed to open file: %s"):format(err)
+	end
+
+	local content = f:read("*a")
+	f:close()
+	return content
+end
+
+local function read_json(path)
+	local content, err = read_file(path)
+	if not content then
+		return nil, err
+	end
+
+	local obj, pos, decode_err = json.decode(content, 1, nil)
+	if decode_err then
+		return nil, ("Invalid JSON: %s"):format(decode_err)
+	end
+
+	return obj
+end
+
+function GetEntries()
+	local config_map = "/home/arch/.local/share/arch-dotfiles/config/.config/walker/json/neovim_lsp.json"
+	local entries = {}
+
+	local entries, err = read_json(config_map)
+	if not entries then
+		error(err)
+	end
+
+	return entries
+end
